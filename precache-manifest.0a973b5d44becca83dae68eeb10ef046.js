ff3994b5ce891787476ad530c0a9ee6915ce3ae2
Oded BD <oded@permit.io>
2022-01-10 23:41:17 +0200

    update name

---
+++ precache-manifest.0a973b5d44becca83dae68eeb10ef046.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0eb848e42732c40cfe81",
+    "url": "/typersist/css/about.89b1d103.css"
+  },
+  {
+    "revision": "b6788baa56e0f682deb5",
+    "url": "/typersist/css/app.f2b5f3de.css"
+  },
+  {
+    "revision": "130d5ca222c36bc32c724007760cfe6e",
+    "url": "/typersist/index.html"
+  },
+  {
+    "revision": "0eb848e42732c40cfe81",
+    "url": "/typersist/js/about.63a7f120.js"
+  },
+  {
+    "revision": "b6788baa56e0f682deb5",
+    "url": "/typersist/js/app.f6729976.js"
+  },
+  {
+    "revision": "5bcc3d239f1378030278",
+    "url": "/typersist/js/chunk-vendors.201cdf98.js"
+  },
+  {
+    "revision": "19abcd5892aff7ce37cda142fec3131a",
+    "url": "/typersist/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/typersist/robots.txt"
+  }
+]);
